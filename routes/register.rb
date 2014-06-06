0ae71c1d2452d105577ea18e316a2b8dd0dfb61f
Bryan <bryan.haase@gmail.com>
2014-06-06 16:27:21 +0000

    Registration class

--- routes/register.rb
+++ routes/register.rb
@@ -2,6 +2,14 @@ post '/register/submit' do
   username = params[:username]
   password = params[:password]
   email = params[:email]
+  duplicate = database["SELECT username FROM accounts WHERE username ='#{username}'"]
+  if duplicate.count > 0
+    return return "{\"result\": \"fail\", \"error\": \"Username already in Use`\"}"
+  end 
+  duplicate = database["SELECT email FROM accounts WHERE email ='#{email}'"]
+  if duplicate.count > 0
+    return return "{\"result\": \"fail\", \"error\": \"Email already registered`\"}"
+  end
   key = KeyGenerator.generate
   database[:accounts].insert(:account_name => username, :password => password, :email => email, :key => key, :created_at => Time.now)
   return "{\"result\": \"ok\", \"key\": \"#{key}\"}"
