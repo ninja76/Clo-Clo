35ef025aa1fc61eaad4efc2ba89692e8debf111a
Bryan <bryan.haase@gmail.com>
2014-06-09 18:11:15 +0000

    asdF

--- routes/api.rb
+++ routes/api.rb
@@ -16,19 +16,23 @@ end
 
 get '/create/:nodeID' do
   # Update Time stamp
-  insert = $redis.hset(params[:nodeID], "updated_time", Time.now.to_f)
+  now = Time.now.to_f
+  h = {} 
   params.keys.each do |k|
-   value = "{\"#{k}\":\"#{params[k]}\"}"
-   if k != "splat" and k != "captures" and k != "geo"
-     $redis.hset(params[:nodeID], k, params[k])
+   if k != "splat" and k != "captures" and k != "geo" and k != "nodeID"
+    h[params[k]] = k
    end
    if k == "geo" and params[k] == "yes"
      geo = JSON.parse(geolookup(request.ip))
-     $redis.hset(params[:nodeID], "latitude", geo["lat"])
-     $redis.hset(params[:nodeID], "longitude", geo["long"])
-     puts "Inserting Geo Data #{geo["lat"]}, #{geo["long"]} "
+#     $redis.hset(params[:nodeID], "latitude", geo["lat"])
+#     $redis.hset(params[:nodeID], "longitude", geo["long"])
+#     puts "Inserting Geo Data #{geo["lat"]}, #{geo["long"]} "
    end
   end
+  h[now] = "updated"
+  my_a = h.to_a
+  puts my_a.inspect
+  $redis.zadd(params[:nodeID],my_a)
   $redis.expire(params[:nodeID],86400)
   content_type :json
   return '{"result": "success"}' 
