92c80504ed9ed12218d1ddf1f2bf56db57d41bfc
Bryan <bryan.haase@gmail.com>
2014-06-10 16:02:14 +0000

    Removed email on registration form

--- helpers/main.rb
+++ helpers/main.rb
@@ -8,7 +8,7 @@ end
 
 class KeyGenerator
   require "digest/sha1"
-  def self.generate(length = 8)
+  def self.generate(length = 12)
     Digest::SHA1.hexdigest(Time.now.to_s + rand(12341234).to_s)[1..length]
   end
 end
--- routes/register.rb
+++ routes/register.rb
@@ -1,18 +1,19 @@
 post '/register/submit' do
   username = params[:username]
   password = params[:password]
-  email = params[:email]
+  #email = params[:email]
+  email = ""
   duplicate = database["SELECT account_name FROM accounts WHERE account_name ='#{username}'"]
   if duplicate.count > 0
     return "{\"result\": \"fail\", \"error\": \"Username already in Use\"}"
   end 
-  duplicate = database["SELECT email FROM accounts WHERE email ='#{email}'"]
-  if duplicate.count > 0
-    return "{\"result\": \"fail\", \"error\": \"Email already registered\"}"
-  end
-  if emailValidate(email) == 1
-    return "{\"result\": \"fail\", \"error\": \"Invalid email address\"}"
-  end
+#  duplicate = database["SELECT email FROM accounts WHERE email ='#{email}'"]
+#  if duplicate.count > 0
+#    return "{\"result\": \"fail\", \"error\": \"Email already registered\"}"
+#  end
+#  if emailValidate(email) == 1
+#    return "{\"result\": \"fail\", \"error\": \"Invalid email address\"}"
+#  end
   key = KeyGenerator.generate
   database[:accounts].insert(:account_name => username, :password => password, :email => email, :key => key, :created_at => Time.now)
   return "{\"result\": \"ok\", \"key\": \"#{key}\"}"
