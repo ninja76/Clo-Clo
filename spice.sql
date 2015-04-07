8eb7ed15bc6306a05f64fe08e298a9e33e127c5b
Bryan <bryan.haase@gmail.com>
2015-04-07 14:36:08 +0000

    cleanup

--- spice.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO streams VALUES(2,1,0,'Des Moines Iowa Weather','Arduino based weather sensor project','2015-03-12 20:08:40.079583',1427583007,NULL);
-INSERT INTO streams VALUES(3,1,0,'clo-clo Webserver Metrics','Various system metrics','2015-03-19 20:50:07.145468',1427583003,NULL);
-COMMIT;
